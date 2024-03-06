e9175edfda1bae9897773bbd349fa6accd7c0269
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2024-03-06 02:20:18 -0500

    Updating package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LanguagePage from "./core/pages/LanguagesPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LanguagePage/>}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LanguagePage/>
   );
 }
 
