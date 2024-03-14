7cfb96d795b8c313c5ae730ec077515b0526c7d7
Julian Alexander Murillo <julian.alexander.murillo@gmail.com>
2024-03-13 20:49:48 -0500

    BUGFIX: Reinstating the languages login

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SignInUp from "./UI/SignUp/SignUp";
 import LanguagePage from "./core/pages/LanguagesPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <LanguagePage/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SignInUp disableLogin={process.env.REACT_APP_DISABLE_LOGIN === 'true'} />} />
+        <Route path="/languages" element={<LanguagePage/>}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
